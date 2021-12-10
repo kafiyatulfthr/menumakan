dd82a609be491ef67cc3faaeeb68351f62d7caf4
Kafiya <fiyakafiya@gmail.com>
2021-12-10 16:10:09 +0700

    add comment

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from MySQLdb.cursors import Cursor
 from flask import Flask, render_template, url_for, request, redirect, make_response, Response
 from flask_sqlalchemy import SQLAlchemy
 import csv
@@ -7,8 +6,10 @@ from io import StringIO
 import io
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/menumakan'
+
+#database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' #connect ke local directory
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/menumakan' #connect ke local server
 db = SQLAlchemy(app)
 
 class listmenu(db.Model):
@@ -21,9 +22,8 @@ class listmenu(db.Model):
     def __repr__(self):
         return '<menu %r>' % self.id
 
-def transform(text_file_contents):
-    return text_file_contents.replace("=", ",")
-
+###############################################################
+#INPUT menu through form
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
@@ -41,11 +41,13 @@ def index():
         except:
             return 'There was an issue adding your menu'
 
+#Read menu from database
     else:
         menus = listmenu.query.order_by(listmenu.id).all()
         return render_template('index.html', menus=menus)
 
 ###############################################################
+#READ menu from file then INPUT/insert to database
 @app.route('/uploadfiles', methods=['POST', 'GET'])
 # Get the uploaded files
 def uploadfiles():
@@ -53,14 +55,16 @@ def uploadfiles():
         csv_file = request.files['file']
         csv_file = TextIOWrapper(csv_file, encoding='utf-8')
         csv_reader = csv.reader(csv_file, delimiter=',')
+        next(csv_reader)
         for row in csv_reader:
             new_menu = listmenu(content=row[0], timing=row[1], score=row[2], status=row[3])
             db.session.add(new_menu)
             db.session.commit()
         return redirect('/')
     return render_template('upload-files.html')
-################################################################
 
+################################################################
+# DELETE menu
 @app.route('/delete/<int:id>')
 def delete(id):
     menu_to_delete = listmenu.query.get_or_404(id)
@@ -72,6 +76,8 @@ def delete(id):
     except:
         return 'There was a problem deleting that menu'
 
+###############################################################
+#UPDATE menu
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     menu = listmenu.query.get_or_404(id)
@@ -91,8 +97,8 @@ def update(id):
     else:
         return render_template('update.html', menu=menu)
 
-
 ####################################################################################
+#COMBINE through scoring and restore it into a database
 @app.route('/scoring', methods=['GET'])
 def scoring():
         menus = listmenu.query.filter_by(status = 'Active').all()
@@ -115,7 +121,7 @@ def updatescore(id):
         return render_template('score_update.html', menu=menu)
 
 ########################################################################
-#Download Report to CSV
+#WRITE database into a file
 @app.route('/download')
 def download():
     menu = db.session.query(listmenu.id, listmenu.content, listmenu.timing, listmenu.score, listmenu.status)
